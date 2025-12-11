1ff8e9150c12f849041f6d51c2a030ef40e22097
Grigory <zvgrih@gmail.com>
2025-12-12 01:20:34 +0400

    Знакомство с PageObject. Практика работы с Page Object

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,12 +1,13 @@
 import pytest
 from playwright.sync_api import Page
 
-from pages.registration_page import RegistrationPage, DashboardPage
+from pages.dashboard_page import DashboardPage
+from pages.registration_page import RegistrationPage
 
 
 @pytest.mark.regression  # Добавили маркировку regression
 @pytest.mark.registration
-def test_successful_registration(chromium_page: Page, registration_page: RegistrationPage, dashboard_page: DashboardPage):  # Создаем тестовую функцию
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):  # Создаем тестовую функцию
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
     registration_page.fill_registration_form('user@gmail.com', 'password', 'username')
     registration_page.click_registration_button()
